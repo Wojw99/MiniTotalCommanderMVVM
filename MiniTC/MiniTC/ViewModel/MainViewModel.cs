726fb3f6c54e93501483ba3a02a73ca61eb94444
Wojw99 <42806302+Wojw99@users.noreply.github.com>
2020-05-21 10:56:37 +0200

    Big Commit

---
+++ MiniTC/MiniTC/ViewModel/MainViewModel.cs
@@ -0,0 +1,128 @@
+﻿using MiniTC.ViewModel.BaseClasses;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Resources;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace MiniTC.ViewModel
+{
+    using R = Properties.Resources;
+    class MainViewModel : BaseViewModel
+    {
+        public Panel LeftPanel { get; set; }
+        public Panel RightPanel { get; set; }
+
+        public MainViewModel()
+        {
+            LeftPanel = new Panel();
+            RightPanel = new Panel();
+        }
+
+        public void Copy(object sender)
+        {
+            string destination = RightPanel.Path;
+            string[] leftPath = LeftPanel.Path.Split('\\');
+
+            destination += '\\';
+            destination += leftPath[leftPath.Length - 1];
+
+            try
+            {
+                if (!File.Exists(destination))
+                {
+                    File.Copy(LeftPanel.Path, destination);
+                    RightPanel.UpdatePathAndContent(new object());
+                    MessageBox.Show($"{R.Copied} {RightPanel.Path}");
+                }
+                else
+                {
+                    MessageBox.Show(R.NotExist);
+                }
+            }
+            catch(UnauthorizedAccessException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        #region Commands
+        private ICommand _rightSelectionChanged = null;
+        public ICommand RightSelectionChanged
+        {
+            get 
+            { 
+                if(_rightSelectionChanged == null)
+                {
+                    _rightSelectionChanged = new RelayCommand(RightPanel.UpdatePathAndContent, arg => true);
+                }
+
+                return _rightSelectionChanged; 
+            }
+        }
+
+        private ICommand _rightDriverChanged = null;
+        public ICommand RightDriverChanged
+        {
+            get 
+            { 
+                if(_rightDriverChanged == null)
+                {
+                    _rightDriverChanged = new RelayCommand(RightPanel.SetDefaultPath, arg => true);
+                }
+                return _rightDriverChanged; 
+            }
+        }
+
+
+        private ICommand leftDriverChanged = null;
+        public ICommand LeftDriverChanged
+        {
+            get 
+            { 
+                if(leftDriverChanged == null)
+                {
+                    leftDriverChanged = new RelayCommand(LeftPanel.SetDefaultPath, arg => true);
+                }
+                return leftDriverChanged; 
+            }
+        }
+
+        private ICommand leftSelectionChanged = null;
+        public ICommand LeftSelectionChanged
+        {
+            get
+            {
+                if (leftSelectionChanged == null)
+                {
+                    leftSelectionChanged = new RelayCommand(LeftPanel.UpdatePathAndContent, arg => true);
+                }
+                return leftSelectionChanged;
+            }
+        }
+
+        private ICommand copyClick = null;
+        public ICommand CopyClick
+        {
+            get
+            {
+                if (copyClick == null)
+                {
+                    copyClick = new RelayCommand(
+                        Copy, 
+                        arg => File.Exists(LeftPanel.ListSelected) && Directory.Exists(RightPanel.Path)
+                        );
+                }
+                return copyClick;
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ MiniTC/MiniTC/ViewModel/Panel.cs
@@ -0,0 +1,152 @@
+﻿using MiniTC.ViewModel.BaseClasses;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace MiniTC.ViewModel
+{
+    class Panel : BaseViewModel
+    {
+        private string path;
+        private string driveSelected;
+        private string listSelected;
+        private ObservableCollection<string> contentCollection;
+
+        public Panel()
+        {
+            DriveArray = Directory.GetLogicalDrives();
+            DriveSelected = DriveArray[0];
+
+            Path = DriveSelected;
+            ContentCollection = LoadContentCollection(DriveSelected);
+        }
+
+        #region Get/Set
+        public string Path
+        {
+            get
+            {
+                return path;
+            }
+            set
+            {
+                path = value; OnPropertyChanged(nameof(Path));
+            }
+        }
+
+        public string DriveSelected
+        {
+            get
+            {
+                return driveSelected;
+            }
+            set
+            {
+                driveSelected = value; OnPropertyChanged(nameof(DriveSelected));
+            }
+        }
+
+        public string ListSelected
+        {
+            get
+            {
+                return listSelected;
+            }
+            set
+            {
+                listSelected = value; OnPropertyChanged(nameof(ListSelected));
+            }
+        }
+
+        public string[] DriveArray { get; set; }
+        public ObservableCollection<string> ContentCollection
+        {
+            get => contentCollection; 
+            set
+            {
+                contentCollection = value; OnPropertyChanged(nameof(ContentCollection));
+            }
+        }
+        #endregion
+
+        public ObservableCollection<string> LoadContentCollection(string path)
+        {
+            try
+            {
+                var collection = new ObservableCollection<string>();
+                var directories = Directory.GetDirectories(path);
+                var files = Directory.GetFiles(path);
+
+                if (!DriveArray.Contains(path))
+                {
+                    collection.Add("..");
+                }
+
+                for (int i = 0; i < directories.Length; i++)
+                {
+                    collection.Add("<D>" + directories[i]);
+                }
+
+                for (int i = 0; i < files.Length; i++)
+                {
+                    collection.Add(files[i]);
+                }
+
+                return collection;
+            } 
+            catch (UnauthorizedAccessException ex)
+            {
+                MessageBox.Show(ex.Message);
+                SetDefaultPath(new object());
+                return ContentCollection;
+            }
+        }
+
+        public void SetDefaultPath(object sender)
+        {
+            Path = DriveSelected;
+            ContentCollection = LoadContentCollection(DriveSelected);
+        }
+
+        public void UpdatePathAndContent(object sender)
+        {
+            if (ListSelected != null)
+            {
+                if (ListSelected == "..")
+                {
+                    var tmp = File.GetAttributes(Path);
+                    var parent = Directory.GetParent(Path);
+
+                    if (tmp.HasFlag(FileAttributes.Directory))
+                    {
+                        Path = parent.FullName;
+                        ContentCollection = LoadContentCollection(Path);
+                    }
+                    else
+                    {
+                        parent = Directory.GetParent(parent.FullName);
+                        Path = parent.FullName;
+                        ContentCollection = LoadContentCollection(Path);
+                    }
+
+                    ListSelected = null;
+                }
+                else if (ListSelected[0] == '<')
+                {
+                    Path = ListSelected.Substring(3);
+                    ContentCollection = LoadContentCollection(Path);
+                }
+                else
+                {
+                    Path = ListSelected;
+                }
+            }
+        }
+    }
+}
